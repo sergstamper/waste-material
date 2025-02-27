dd07d72c967c4128215a73024eaf280e2b45d592
sergstamper <serg.kabanoff@gmail.com>
2025-02-28 01:15:05 +0200

    Forming resul function

--- src/components/functions/calcWaste.js
+++ src/components/functions/calcWaste.js
@@ -1,15 +1,12 @@
 import filterMinWaste from "./filterMinWaste";
+import formingResArr from "./formingResArr";
 
 function calcWaste(width, height, material) {
   const currentSizeArr = material.size;
   const length = currentSizeArr.length-1;
   let currentWidth = +width;
   let currentHeight = +height;
-  let resultWidth = 0;
-  let resultHeight = 0;
   let repeat = 1
-  let wasteInWidthArr = [];
-  let wasteInHeightArr = [];
 
   if (currentWidth > +currentSizeArr[length] 
     && currentHeight > +currentSizeArr[length]) {
@@ -31,47 +28,8 @@ function calcWaste(width, height, material) {
     currentHeight = temp;
   }
 
-  currentSizeArr.forEach((size) => {
-    repeat = Math.ceil(currentWidth/size);
-    if (repeat > 1) {
-      resultWidth = currentWidth/repeat;
-      resultHeight = currentHeight*repeat;
-    } else {
-      resultWidth = currentWidth;
-      resultHeight = currentHeight;
-    }
-
-    const wasteWidth = size - resultWidth;
-    const wasteAreaInWidth = (wasteWidth / 1000) * (resultHeight / 1000);
-
-    wasteInWidthArr.push({
-      size: size,
-      repeat: repeat,
-      wasteWidth: wasteWidth.toFixed(2),
-      waste: wasteAreaInWidth.toFixed(2)
-    });
-  });
-
-  currentSizeArr.forEach((size) => {
-    repeat = Math.ceil(currentHeight/size);
-    if (repeat > 1) {
-      resultHeight = currentHeight/repeat;
-      resultWidth = currentWidth*repeat;
-    } else {
-      resultHeight = currentHeight;
-      resultWidth = currentWidth;
-    }
-
-    const wasteHeight = size - resultHeight;
-    const wasteAreaInHeight = (wasteHeight / 1000) * (resultWidth / 1000);
-
-    wasteInHeightArr.push({
-      size: size,
-      repeat: repeat,
-      wasteHeight: wasteHeight.toFixed(2),
-      waste: wasteAreaInHeight.toFixed(2)
-    });
-  });
+  const wasteInWidthArr = formingResArr(currentWidth, currentHeight, material, repeat, 'width');
+  const wasteInHeightArr = formingResArr(currentWidth, currentHeight, material, repeat, 'height');
 
   console.log(wasteInWidthArr);
   console.log(wasteInHeightArr);
---
+++ src/components/functions/formingResArr.js
@@ -0,0 +1,59 @@
+function formingResArr(
+  currentWidth, 
+  currentHeight, 
+  material, 
+  repeat, 
+  direction) {
+
+  let width = 0; 
+  let height = 0;
+  let resultWidth = 0;
+  let resultHeight = 0;
+  let wasteArr = [];
+  let key = 'wasteWidth';
+  let currentMaterialType = material.type;
+  let currentSizeArr = material.size;
+
+  if (direction === 'width') {
+    width = currentWidth;
+    height = currentHeight;
+  } else {
+    width = currentHeight;
+    height = currentWidth;
+    key = 'wasteHeight'
+  }
+
+  currentSizeArr.forEach((size) => {
+    repeat = Math.ceil(width/size);
+    let add = 0;
+    if (repeat > 1) {
+      if (currentMaterialType === 'banner') {
+        add = repeat === 2 ? 40 : 80;
+      } else if (currentMaterialType === 'vinyl') {
+        add = repeat === 2 ? 5 : 10;
+      }
+
+      resultWidth = width/repeat + add;
+      resultHeight = height*repeat;
+    } else {
+      resultWidth = width;
+      resultHeight = height;
+    }
+
+    const wasteValue = size - resultWidth;
+    const wasteArea = (wasteValue / 1000) * (resultHeight / 1000);
+
+    if (wasteValue >= 0) {
+      wasteArr.push({
+        size: size,
+        repeat: repeat,
+        [key]: wasteValue.toFixed(2),
+        waste: wasteArea.toFixed(2)
+      });
+    }
+  });
+
+  return wasteArr;
+}
+
+export default formingResArr;
